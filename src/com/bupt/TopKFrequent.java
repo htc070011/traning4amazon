47cf29ff5760ce9814ae24d9c72e16b1d7380d6d
htc070011 <kankanzenmeshuo@163.com>
2018-07-05 22:44:36 +0800

    top k frequent

---
+++ src/com/bupt/TopKFrequent.java
@@ -0,0 +1,50 @@
+package com.bupt;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+//import static java.util.stream.Collectors.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class TopKFrequent {
+
+    @Test
+    public void test() {
+        int[] nums = {1,1,1,2,2,3};
+        List<Integer> list = topKFrequent(nums, 2);
+        System.out.println(list.size());
+        list.stream().forEach(System.out::println);
+    }
+
+
+
+    public List<Integer> topKFrequent(int[] nums, int k) {
+
+        List<Integer> ret = new ArrayList<>();
+        ArrayList[] arry = new ArrayList[nums.length + 1];
+        for(int i = 0; i < arry.length; i++) {
+            arry[i] = new ArrayList();
+        }
+        Arrays.stream(nums)
+                .boxed()
+                .collect(
+                        Collectors.groupingBy(
+                                Function.identity(), Collectors.collectingAndThen(Collectors.reducing(Integer::sum), Optional::get)
+                        )
+                )
+                .entrySet()
+                .stream()
+                .forEach(x -> arry[x.getValue()].add(x));
+        for(int i = arry.length - 1; i > nums.length - k && ret.size() < k; i--) {
+            if(arry[i] != null)
+                ret.addAll(arry[i]);
+        }
+
+        return ret;
+    }
+}
